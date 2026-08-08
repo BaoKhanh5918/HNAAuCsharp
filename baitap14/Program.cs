7a687cc7494f24d353177b3ee6cfcc31f2565c91
BaoKhanh5918 <you@example.com>
2026-08-08 22:40:53 +0700

    Initial commit

---
+++ baitap14/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+class Program
+{
+    static void Main()
+    {
+        ArrayList ds = new ArrayList();
+        ds.Add("An");
+        ds.Add(25);  
+        ds.Insert(1, "Binh");
+        ds.Remove("An"); 
+        ds.RemoveAt(0); 
+        ds.Add("Chau");
+        string ten = (string)ds[0];
+        Console.WriteLine("Tên lấy từ ArrayList: " + ten);
+    }
+}
